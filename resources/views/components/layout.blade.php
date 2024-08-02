20c5a067702e60bd3c47b523b2b5844d26be427a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-03 00:28:29 +0100

    Installed TailwindCSS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Default Title')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
 </head>
 <body>
     <header>
@@ -12,10 +14,11 @@
     </header>
 
     <main>
-        @yield('content')
+        {{ $slot}}
     </main>
 
     <footer>
+
 
     </footer>
 
