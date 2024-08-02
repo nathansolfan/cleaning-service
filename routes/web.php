43f9fcfc5c55b141febabf50848a0deb5ba0cbc7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-02 16:18:41 +0100

    delete route and userController where we have the destroy() func at the end

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,13 +12,17 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
+// LOGIN
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login'])->name('login.post');
 
+
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-
+// REGISTER
 Route::get('register',[RegisterController::class, 'showRegisterForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register.post');
 
+// DELETE
+Route::delete('user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
