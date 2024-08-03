5accb7c810258895ebb50c713344a43e530f10bf
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-03 14:50:15 +0100

    create route for bookings also its index.blade.php and the bookingController index() func creating a $booking var from the model Booking::all() and return the view() to 'bookings.index'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
@@ -15,18 +16,18 @@ Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware(
 // LOGIN
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login'])->name('login.post');
-
-
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 // REGISTER
 Route::get('register',[RegisterController::class, 'showRegisterForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register.post');
 
-
 // USERS - resource gets index/create/store/show/edit/update/destroy
 Route::resource('users', UserController::class);
 
 // DELETE
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+// BOOKING
+Route::resource('bookings', BookingController::class);
 
