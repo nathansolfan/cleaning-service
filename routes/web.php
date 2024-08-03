8b500f036809b3927c5eec19d44bd3b324e97c69
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-03 11:18:44 +0100

    updatesand fixing folders

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
